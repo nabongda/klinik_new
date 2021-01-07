a588db8d759ed622eb6f7d1e625eb326c93c1737
nurlaelakhasannah <nurlaelakhsnnh12@gmail.com>
2021-01-07 13:30:35 +0700

    Rename Pelayanan

--- redaktur/modul/pelayanan_obat/pelayanan_obat.php
+++ redaktur/modul/pelayanan_obat/pelayanan_obat.php
@@ -11,12 +11,12 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Data Pelayanan Obat</h1>
+        <h1>Data Penjualan Obat</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="?module=home">Beranda</a></li>
-          <li class="breadcrumb-item active">Data Pelayanan Obat</li>
+          <li class="breadcrumb-item active">Data Penjualan Obat</li>
         </ol>
       </div>
     </div>
@@ -32,7 +32,7 @@
           <div class="row">
             <div class="col-md-12">
               <div class="col-md-12"><a href="?module=pelayanan_obat&act=tambah" class="btn btn-primary">Tambah Data
-                  Pelayanan</a>
+                  Penjualan</a>
               </div>
             </div>
           </div>
